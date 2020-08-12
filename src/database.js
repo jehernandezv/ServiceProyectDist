57bf6f4e71819ad312acd220b08f64db0daa2fac
unknown <cristian.lopez04@uptc.edu.co>
2020-08-12 18:13:21 -0500

    Se realiza cambio de base de datos local a cloud db

--- src/database.js
+++ src/database.js
@@ -1,6 +1,16 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://root:123456@192.168.0.114:27017/projectFinal', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('db is conected')).catch(err => console.log(err));
+}).then(() => console.log('db is conected')).catch(err => console.log(err));*/
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kamil018:12345@dbprojectone.jxqa9.mongodb.net/projectFinal?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("patients");
+  // perform actions on the collection object
+  client.close();
+});
