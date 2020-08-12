a325df945b5e848c63a11001187ce851f4777185
unknown <cristian.lopez04@uptc.edu.co>
2020-08-12 01:05:42 -0500

    Se agrega metodo get

--- src/index.js
+++ src/index.js
@@ -9,22 +9,10 @@ var app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-<<<<<<< HEAD
-app.use(body_parser.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(body_parser.json());
-
-/*
-app.post('/addCovid', async (req,res) => {
-    try{
-        const user = await req.body.user;
-        console.log(req);
-        console.log(user);
-    }catch(e){
-        console.log(e);
-        res.json(e.message);
-    }
-});*/
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/:city',async function(req,res){
     console.log(req.params.city);
@@ -32,11 +20,6 @@ app.get('/:city',async function(req,res){
         message: 'get de cuidad del server: ' + numServer
     });
 });
-=======
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
 
 app.post('/test', function (req, res) {
     if (!req.body)
@@ -46,7 +29,6 @@ app.post('/test', function (req, res) {
     var user = req.body.user;
     res.send('Bienvenido ' + user)
 })
->>>>>>> 1a4d67f3f0de62dd31ee0acfa179be1e59c00700
 
 app.listen(port, function () {
     console.log('Example app server ubuntu1 listening on port ' + port + '!');
