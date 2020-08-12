1a4d67f3f0de62dd31ee0acfa179be1e59c00700
unknown <cristian.lopez04@uptc.edu.co>
2020-08-11 22:20:16 -0500

    Cambios en el servicio, minimos.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var morgan = require('morgan');
-var body_parser = require('body-parser');
+var bodyParser = require('body-parser');
 var port = process.argv[2];
 var numServer = process.argv[3];
 
@@ -9,18 +9,19 @@ var app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(body_parser.urlencoded({ extended: false }));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
-app.get('/', async (req,res) => {
-    try{
-    await res.json({
-        message: 'El servidor ' + numServer+  ' responde '
-    });
-    }catch(e){
-        console.log(e);
-        res.json(e.message);
-    }
-});
+app.post('/test', function (req, res) {
+    if (!req.body)
+        return res.sendStatus(400)
+    console.log(req.body);
+
+    var user = req.body.user;
+    res.send('Bienvenido ' + user)
+})
 
 app.listen(port, function () {
     console.log('Example app server ubuntu1 listening on port ' + port + '!');
