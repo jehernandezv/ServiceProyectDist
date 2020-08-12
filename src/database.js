b1a0703689c5114acd23a8129d430f043cb980c0
Camilo018 <cristian.lopez04@uptc.edu.co>
2020-08-12 16:22:18 -0500

    s

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://kamil018:12345@192.168.1.32:27017/projectFinal', {
+mongoose.connect('mongodb://superAdmin:pass1234@192.168.0.114:27017/projectFinal', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('db is conected')).catch(err => console.log(err));
