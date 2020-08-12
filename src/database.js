3cff3d6ac8f6927709b2e249685f7a812e8291b4
Camilo018 <cristian.lopez04@uptc.edu.co>
2020-08-12 17:32:30 -0500

    algo

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://superAdmin:pass1234@192.168.0.114:27017/projectFinal', {
+mongoose.connect('mongodb://root:123456@192.168.0.114:27017/projectFinal', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('db is conected')).catch(err => console.log(err));
